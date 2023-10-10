3c90ce494d717e045164d49a55319afb8c2eb887
DanielRiosM <56373511+DanielRiosM@users.noreply.github.com>
2023-10-10 12:14:51 -0700

    Ej1: Menu avanzado

--- src/scripts/script.js
+++ src/scripts/script.js
@@ -1,4 +1,4 @@
-let slideIndex = 1;
+let slideIndex = 0;
         showSlides();
 
         function showSlides() {
@@ -7,17 +7,35 @@ let slideIndex = 1;
 
             for (i = 0; i < slides.length; i++) {
                 slides[i].style.display = "none";
-                
             }
+
+            slideIndex++;
 
             if (slideIndex > slides.length) {
                 slideIndex = 1;
             }
 
             slides[slideIndex - 1].style.display = "block";
-            
+            setTimeout(showSlides, 4000); // Cambia la imagen cada 2 segundos
         }
 
         function plusSlides(n) {
             showSlides(slideIndex += n);
         }
+
+        document.addEventListener("DOMContentLoaded",() => {
+            const scrollToBottomButtom = document.getElementById('btn-ir-arriba');
+            window.addEventListener("scroll",() => {
+                if(window.scrollY > 250){
+                    scrollToBottomButtom.style.display = 'block';
+                }else{
+                    scrollToBottomButtom.style.display = 'none';
+                }
+            })
+            scrollToBottomButtom.addEventListener('click',() =>{
+                window.scrollTo({
+                    top: 0,
+                    behavior: 'smooth'
+                })
+            })
+        })
