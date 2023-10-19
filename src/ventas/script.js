41791f81661f05e704640ed82692e05bebfc71a0
DanielRiosM <56373511+DanielRiosM@users.noreply.github.com>
2023-10-19 12:14:12 -0700

    Ej2: Get

--- src/ventas/script.js
+++ src/ventas/script.js
@@ -1,5 +1,80 @@
+document.addEventListener("DOMContentLoaded", function () {
+    const dataTableBody = document.getElementById("data-table-body");
+    const message = document.getElementById("message");
+    const filterForm = document.getElementById("filter-form");
+
+    // Agrega un evento de escucha al formulario para manejar la solicitud de filtro.
+    filterForm.addEventListener("submit", function (e) {
+        e.preventDefault();
+        fetchData();
+    });
+
+    // Realiza la solicitud Fetch para obtener datos de la API.
+    function fetchData() {
+        // Reemplaza la URL de la API con la URL real.
+        const apiUrl = "https://653039776c756603295e6dda.mockapi.io/ventas"; // Cambia esto con la URL de tu API.
+        
+        fetch(apiUrl)
+            .then((response) => response.json())
+            .then((data) => {
+                if (data.length > 0) {
+                    displayData(data);
+                } else {
+                    displayNoDataMessage();
+                }
+            })
+            .catch((error) => {
+                console.error("Error al obtener los datos:", error);
+            });
+    }
+
+    // Llena la tabla con los datos obtenidos de la API.
+    function displayData(data) {
+        dataTableBody.innerHTML = "";
+        data.forEach((venta) => {
+            const row = document.createElement("tr");
+            row.innerHTML = `
+                <td>${venta.id}</td>
+                <td>${venta.titulo}</td>
+                <td>${venta.plataforma}</td>
+                <td>${venta.cliente}</td>
+                <td class="text-right">${venta.precio}</td>
+                <td>
+                <button class="btn-delete" data-sale-id="${venta.id}">Eliminar</button>
+                </td>
+            `;
+            dataTableBody.appendChild(row);
+        });
+        
+        initDeleteSaleButtonHandler();
+    }
+
+    // Muestra un mensaje cuando no hay datos.
+    function displayNoDataMessage() {
+        dataTableBody.innerHTML = "";
+        message.style.display = "block";
+    }
+
+    // Inicialmente, cargar los datos al cargar la página.
+    fetchData();
+});
 
 
+
+function initDeleteSaleButtonHandler() {
+
+    document.querySelectorAll('.btn-delete').forEach(button => {
+
+        button.addEventListener('click', () => {
+
+            const saleId = button.getAttribute('data-sale-id'); // Obtenemos el ID de la venta
+            deleteSale(saleId); // Llamamos a la función para eleminar la venta
+
+        });
+
+    });
+
+}
 
 // Validar el inicio de sesión (debes implementar la lógica real aquí)
 const loginForm = document.getElementById("login-form");
